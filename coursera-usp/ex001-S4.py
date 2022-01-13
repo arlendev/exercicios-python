b28cac841412cf2b478c61150d5be5d8f5037c68
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-13 11:48:48 +0100

    up

---
+++ coursera-usp/ex001-S4.py
@@ -0,0 +1,11 @@
+# Escreva um programa que receba um número natural  n na entrada e imprima  n!(fatorial) na saída.
+
+c = 1  #contador
+fat = 1  
+n = int(input("Digite o valor de n: "))
+
+while c <= n:
+    fat = fat * c
+    c = c + 1
+
+print(fat)
