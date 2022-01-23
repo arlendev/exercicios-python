01b7e356653224e215953622329cecb24453d06e
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-23 13:32:22 +0100

    up

---
+++ desafios-curso-em-video/desafio_107.py
@@ -0,0 +1,8 @@
+from ex107_modulo import moeda
+
+valor = float(input('Informe um valor: R$'))
+
+print(f'Aumento de 10%: {moeda.aumentar(valor, 10):.2f}')
+print(f'Rezudino 15 %: {moeda.diminuir(valor, 15):.2f}')
+print(f'O dobro de {valor:.2f} é: {moeda.dobro(valor):.2f}')
+print(f'A metade de {valor:.2f} é: {moeda.metade(valor):.2f}')
