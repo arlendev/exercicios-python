4e053981598e14ad6b3299abd8efd6cae3b833fc
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-25 14:01:42 +0100

    up

--- intensivao-python-lira/aula01.py
+++ intensivao-python-lira/aula01.py
@@ -8,10 +8,10 @@ pyautogui.PAUSE = 1
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
-pyautogui.hotkey("ctrl", "t")
-pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
+pyautogui.hotkey('ctrl', 't')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
 
 time.sleep(5)
 
@@ -34,7 +34,7 @@ time.sleep(5) # Esperar o download
 import pandas as pd
 
 tabela = pd.read_excel
-tabela = pd.read_excel(r"C:\Users\......") # caminho do xlsx
+tabela = pd.read_excel(r'C:\Users\......') # caminho do xlsx
 print(tabela)
 faturamento = tabela['Valor Final'].sum()
 quantidade = tabela['Quantidade'].sum()
@@ -43,39 +43,39 @@ print(f'O faturamento totao foi R${faturamento:,.2f}')
 print(f'A quantidade de produtos vendidos foi {quantidade}')
 
 # Passo 5: Entrar no email
-pyautogui.hotkey("ctrl", "t")
-pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
+pyautogui.hotkey('ctrl', 't')
+pyperclip.copy('https://mail.google.com/mail/u/0/#inbox')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
 time.sleep(5)
 
 # Passo 6: Enviar por e-mail o resultado
 pyautogui.click(x=109, y=183)
 
-pyautogui.write("xyz@gmail.com") # colocar o e-mail de destino
-pyautogui.press("tab") # seleciona o email
+pyautogui.write('xyz@gmail.com') # colocar o e-mail de destino
+pyautogui.press('tab') # seleciona o email
 # escreve outro email
 # tab
 # escreve outro email
 # tab
-pyautogui.press("tab") # pula pro campo de assunto
-pyperclip.copy("Relatório de Vendas")
-pyautogui.hotkey("ctrl", "v") # escrever o assunto
-pyautogui.press("tab") #pular pro corpo do email
+pyautogui.press('tab') # pula pro campo de assunto
+pyperclip.copy('Relatório de Vendas')
+pyautogui.hotkey('ctrl', 'v') # escrever o assunto
+pyautogui.press('tab') #pular pro corpo do email
 
-texto = f"""
+texto = f'''
 Prezados, bom dia
 
 O faturamento de ontem foi de: R${faturamento:,.2f}
 A quantidade de produtos foi de: {quantidade:,}
 
 Abs
-Arlen Possamai"""
+Arlen Possamai'''
 
 pyperclip.copy(texto)
-pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey('ctrl', 'v')
 
 # clicar no botão enviar
 
 # apertar Ctrl Enter
-pyautogui.hotkey("ctrl", "enter")
+pyautogui.hotkey('ctrl', 'enter')
