4a7f46b82ea479464f24a57e2a8b1b09677a06e8
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-26 11:42:05 +0100

    up

--- intensivao-python-lira/aula02.py
+++ intensivao-python-lira/aula02.py
@@ -4,6 +4,8 @@ import pandas as pd
 tabela = pd.read_csv('telecom_users.csv')
 print(tabela)
 
+tabela = tabela.drop('Unnamed: 0', axis = 1)
+print(tabela)
 # Passo 2: Visualizar a base de dados
 # - Entender quais as informações tão disponíveis
 # - Descobrir as cagadas da base de dados
