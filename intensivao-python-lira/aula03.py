9332c05ea2c2291af471b8091e5cf970d4880f5f
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-28 15:19:59 +0100

    up

--- intensivao-python-lira/aula03.py
+++ intensivao-python-lira/aula03.py
@@ -24,7 +24,16 @@ cotacao_dolar = navegador.find_element(By.XPATH,
     '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value") 
 print(cotacao_dolar)
 
-# Passo 2: Visualizar a base de dados
+# Passo 2: Pegar a cotação do Euro
+navegador.get("https://www.google.com/")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_euro = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+print(cotacao_euro)
 
 # Passo 3: Tratamento de dados
 
