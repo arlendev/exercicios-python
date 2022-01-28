7f3388e22d85d6f3213a521d613fe22d1040199a
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-01-28 18:15:51 +0100

    up

--- intensivao-python-lira/aula03.py
+++ intensivao-python-lira/aula03.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pyautogui
+import time
 
 # para rodar o chrome em 2º plano
 # from selenium.webdriver.chrome.options import Options
@@ -9,40 +11,42 @@ from selenium.webdriver.common.by import By
 # navegador = webdriver.Chrome(options=chrome_options)
 
 # abrir um navegador
-navegador = webdriver.Chrome("chromedriver.exe")
+navegador = webdriver.Chrome()
 
 navegador.get("https://www.google.com/")
+pyautogui.click(x=804, y=923, clicks=1)
 
-# # Passo 1: Pegar a cotação do Dólar
-# navegador.find_element(By.XPATH,
-#     '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dólar")
 
-# navegador.find_element(By.XPATH,
-#     '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+# Passo 1: Pegar a cotação do Dólar
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dólar")
 
-# cotacao_dolar = navegador.find_element(By.XPATH,
-#     '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value") 
-# print(cotacao_dolar)
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 
-# # Passo 2: Pegar a cotação do Euro
-# navegador.get("https://www.google.com/")
-# navegador.find_element(By.XPATH,
-#     '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
-# navegador.find_element(By.XPATH,
-#     '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+cotacao_dolar = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value") 
+print(cotacao_dolar)
 
-# cotacao_euro = navegador.find_element(By.XPATH,
-#     '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
-# print(cotacao_euro)
+# Passo 2: Pegar a cotação do Euro
+navegador.get("https://www.google.com/")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 
-# # Passo 3: Pegar a cotação do Ouro
-# navegador.get("https://www.melhorcambio.com/ouro-hoje")
+cotacao_euro = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+print(cotacao_euro)
 
-# cotacao_ouro = navegador.find_element(By.XPATH, '//*[@id="comercial"]').get_attribute("value")
-# cotacao_ouro = cotacao_ouro.replace(",", ".")
-# print(cotacao_ouro)
+# Passo 3: Pegar a cotação do Ouro
+navegador.get("https://www.melhorcambio.com/ouro-hoje")
 
-# navegador.quit()
+cotacao_ouro = navegador.find_element(By.XPATH, '//*[@id="comercial"]').get_attribute("value")
+cotacao_ouro = cotacao_ouro.replace(",", ".")
+print(cotacao_ouro)
+
+navegador.quit()
 
 # # Passo 4: Importar a lista de produtos
 # import pandas as pd
