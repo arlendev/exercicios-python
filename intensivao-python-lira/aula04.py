e56f422e1f86006af940117bfc4a79bc2d8eb1d0
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-01 11:56:14 +0100

    up

--- intensivao-python-lira/aula04.py
+++ intensivao-python-lira/aula04.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 tabela = pd.read_csv("advertising.csv")
 print(tabela)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
+plt.show()
