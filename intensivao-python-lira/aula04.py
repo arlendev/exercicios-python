25c5b49a1672a927181f8b62d5732c706f3ae2d6
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-01 13:43:50 +0100

    up

--- intensivao-python-lira/aula04.py
+++ intensivao-python-lira/aula04.py
@@ -8,3 +8,10 @@ import matplotlib.pyplot as plt
 
 sns.heatmap(tabela.corr(), annot=True, cmap="Wistia")
 plt.show()
+
+from sklearn.model_selection import train_test_split
+
+y = tabela["Vendas"]
+x = tabela.drop("Vendas", axis=1)
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
