bc1041cd58a4833b1468c71b15b6e073d419d7a4
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-02 16:16:46 +0100

    up

--- intensivao-python-lira/aula04.py
+++ intensivao-python-lira/aula04.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 tabela = pd.read_csv("advertising.csv")
 print(tabela)
@@ -15,4 +18,8 @@ from sklearn.model_selection import train_test_split
 y = tabela["Vendas"]
 x = tabela.drop("Vendas", axis=1)
 
-x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3)
+
+# cria as inteligencias aritificiais
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
