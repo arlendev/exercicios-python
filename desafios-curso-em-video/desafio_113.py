0f06cf30631b4cd8fa175ee57bf9af2566f39ab8
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-02-10 13:52:41 +0100

    up

--- desafios-curso-em-video/desafio_113.py
+++ desafios-curso-em-video/desafio_113.py
@@ -1,14 +1,37 @@
-def resumo(preco=0, aum=0, red=0):
-    print('--' * 15)
-    print(f'{"RESUMO DO VALOR":^30}')
-    print('--' * 15)
-    res = preco
-    aumento = aum
-    reducao = red
+"""
+Desafio 113
+Problema: Reencreva a função leiaInt() que fizemos no desafio 104, incluindo agora
+          a possibilidade da digitação de um número de tipo inválido.
+          Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
+Resolução do problema:
+"""
 
-    print(f'{"Preço Analisado:":<20} {moeda(res)}')
-    print(f'{"Dobro do Preço:":<20} {moeda(dobro(res))}')
-    print(f'{"Metade do Preço:":<20} {moeda(metade(res))}')
-    print(f'{aum}{"% de aumento:":<18} {moeda(aumentar(res, aumento))}')
-    print(f'{reducao}{"% de redução:":<18} {moeda(diminuir(res, reducao))}')
-    print('--' * 15)
+
+def leiaInt(msg):
+    while True:
+        try:
+            valor_int = int(input(msg))
+            return valor_int
+        except (ValueError, TypeError):
+            print('\n\33[31mERRO! Informe um valor INTEIRO corretamente...\33[m')
+        except KeyboardInterrupt:
+            print('\n\33[31mO usuário não informou esse número...\33[m')
+            return 0
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            valor_float = float(input(msg))
+            return valor_float
+        except (ValueError, TypeError):
+            print('\n\33[31mERRO! Informe um valor FLUTUANTE corretamente...\33[m')
+        except KeyboardInterrupt:
+            print('\n\33[31mO usuário não informou esse número...\33[m')
+            return 0
+
+
+n_int = leiaInt('Digite um valor inteiro: ')
+n_float = leiaFloat('Digite um valor flutuante: ')
+
+print(f'Você digitou como valor inteiro o número: {n_int} e como valor flutuante o número: {n_float}')
